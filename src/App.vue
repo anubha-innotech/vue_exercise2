c6112a9fc8c6b367efbabfae87fe27eea5bb60e2
Anubha <anubha@innotechteam.in>
2022-08-26 12:39:04 +0530

    exercise2

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,103 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="main">
+    <h2>VueJs Exercise 2</h2>
+    <input type="text" ref="input">
+    <button @click="focusBtn">Focus</button>
+    <br />
+    <button @click="show = !show">Modal</button>
+    <div class="prompt" v-if="show">
+      <div id="modal-box">
+        <Modal title="Sign up for the giveaway!" content="Grab your ninja swag for half price!" theme="sale"
+          ref="modal">
+          <template v-slot:links>
+            <a href="#" id="slot-link" @click="show = !show">Close</a>
+          </template>
+        </Modal>
+      </div>
     </div>
-  </header>
-
-  <main>
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    focusBtn() {
+      this.$refs.input.focus();
+      console.log("focus");
+    },
+
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0;
 }
 
-.logo {
+.main {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  padding: 20px;
+}
+
+.prompt {
+  background-color: rgba(0, 0, 0, .3);
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#modal-box {
+  position: relative;
+  background-color: blue;
+}
+
+#slot-link {
+  position: absolute;
   display: block;
-  margin: 0 auto 2rem;
+  right: 40%;
+  margin: 3px;
+  bottom: 0;
+  text-align: center;
+  color: white;
+  background-color: black;
+  padding: 2px 10px;
+  text-decoration: none;
+  font-size: 1.1rem;
+  width: 80px;
+  white-space: nowrap;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+button {
+  margin: 5px 0px;
+  padding: 5px 10px;
+  background-color:dodgerblue;
+  color: white;
+  font-size: 1rem;
+  border: 1px solid dodgerblue;
+}
+input{
+  font-size: 1rem;
+  padding: 5px;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
+
+
---
+++ src/components/Modal.vue
@@ -0,0 +1,62 @@
+  <template>
+  <slot name="links"></slot>
+  <div class="box">
+  <div id="center" :class="{ sale: isValid }">
+    <h1>{{ title }}</h1>
+    <p>{{ content }}</p>
+  </div>
+</div>
+</template>
+  
+<script>
+export default {
+  name:'Modal',
+  data(){
+    return{
+      show:true
+    }
+  },
+  props: {
+    title: String,
+    content: String,
+    theme: String,
+  },
+  created() {
+    if (this.theme == 'sale') {
+      this.isValid = true
+    }
+  },
+}
+
+</script>
+
+<style>
+*{
+  margin: 0px;
+}
+#center {
+  width: 100%;
+  padding: 45px;
+  text-align: center;  
+}
+
+#center h1 {
+  color: rgb(48, 184, 184);
+}
+
+.sale {
+  background-color:orangered !important;
+  color: white;
+}
+
+.sale h1 {
+  color: white !important;
+}
+.box{
+  display: flex;
+  /* justify-content: center; */
+  /* align-items: center; */
+}
+
+
+</style>
